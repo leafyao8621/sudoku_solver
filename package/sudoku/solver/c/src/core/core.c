478317939744c100ed995174b258b588c03237de
Yi Yao <leaf_yao8621@hotmail.com>
2022-04-20 15:27:17 -0700

    FWP c solver

--- package/sudoku/solver/c/src/core/core.c
+++ package/sudoku/solver/c/src/core/core.c
@@ -1,11 +1,99 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include "core.h"
+
+static inline void advance_iter(
+    uint8_t *grid,
+    uint8_t *grid_end,
+    uint8_t **iter
+) {
+    for (++(*iter); (*iter < grid_end) && !(**iter & EMPTY); ++(*iter));
+}
+
+static inline void retreat_iter(uint8_t *grid, uint8_t **iter) {
+    for (--(*iter); (*iter >= grid) && !(**iter & EMPTY); --(*iter));
+}
+
+static inline bool check_position(
+    uint8_t *grid,
+    uint8_t size,
+    uint8_t width,
+    uint8_t *iter
+) {
+    static bool lookup[20];
+    if (!(*iter & ~EMPTY)) {
+        return false;
+    }
+    uint8_t offset = iter - grid;
+    uint8_t row = offset / width;
+    uint8_t col = offset % width;
+    memset(lookup, 0, width);
+    for (uint8_t i = 0, *ii = grid + row * width; i < width; ++i, ++ii) {
+        if (*ii & ~EMPTY) {
+            if (lookup[(*ii & ~EMPTY) - 1]) {
+                return false;
+            }
+            lookup[(*ii & ~EMPTY) - 1] = true;
+        }
+    }
+    memset(lookup, 0, width);
+    for (uint8_t i = 0, *ii = grid + col; i < width; ++i, ii += width) {
+        if (*ii & ~EMPTY) {
+            if (lookup[(*ii & ~EMPTY) - 1]) {
+                return false;
+            }
+            lookup[(*ii & ~EMPTY) - 1] = true;
+        }
+    }
+    memset(lookup, 0, width);
+    for (
+        uint8_t i = 0,
+        *ii = grid + row / size * size * width + col / size * size;
+        i < size;
+        ++i, ii += width - size
+    ) {
+        for (uint8_t j = 0; j < size; ++j, ++ii) {
+            if (*ii & ~EMPTY) {
+                if (lookup[(*ii & ~EMPTY) - 1]) {
+                    return false;
+                }
+                lookup[(*ii & ~EMPTY) - 1] = true;
+            }
+        }
+    }
+    return true;
+}
 
 int core_solve(uint8_t *grid, uint8_t size) {
     if (!grid) {
         return ERR_NULL_PTR;
     }
-    if (!size || size > 20) {
+    if (!size || size > 5) {
         return ERR_INVALID_SIZE;
+    }
+    uint8_t width = size * size;
+    uint8_t *iter = grid;
+    uint8_t *grid_end = grid + size * size * size * size;
+    if (!(*iter & EMPTY)) {
+        advance_iter(grid, grid_end, &iter);
+    }
+    for (; iter < grid_end;) {
+        for (
+            ++(*iter);
+            (*iter & ~EMPTY) <= width &&
+            !check_position(grid, size, width, iter);
+            ++(*iter)
+        );
+        if ((*iter & ~EMPTY) > width) {
+            if (iter == grid) {
+                return ERR_NO_SOLUTION;
+            }
+            *iter = EMPTY;
+            retreat_iter(grid, &iter);
+        } else {
+            advance_iter(grid, grid_end, &iter);
+        }
     }
     return 0;
 }
--- package/sudoku/solver/c/src/test.c
+++ package/sudoku/solver/c/src/test.c
@@ -2,13 +2,18 @@
 #include "core/core.h"
 
 int main(void) {
-    uint8_t grid[16] = {
-        EMPTY, 2, EMPTY, EMPTY,
-        4, 3, 1, EMPTY,
-        3, 1, EMPTY, EMPTY,
-        2, EMPTY, 3, EMPTY
+    uint8_t grid[81] = {
+        EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, 9, 3, EMPTY,
+        EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, 7, EMPTY, EMPTY,
+        EMPTY, EMPTY, 8, EMPTY, EMPTY, 9, 2, 4, 6,
+        8, 5, EMPTY, EMPTY, EMPTY, EMPTY, 6, 2, 4,
+        3, EMPTY, 6, EMPTY, 5, EMPTY, 1, 9, 7,
+        2, EMPTY, 1, 4, EMPTY, EMPTY, EMPTY, EMPTY, 3,
+        EMPTY, EMPTY, 5, EMPTY, 9, EMPTY, 3, EMPTY, 2,
+        9, 2, 3, 7, 8, EMPTY, 4, EMPTY, 1,
+        6, 7, EMPTY, 3, EMPTY, 1, EMPTY, EMPTY, 9
     };
-    int ret = core_solve(grid, 4);
+    int ret = core_solve(grid, 3);
     if (ret) {
         switch (ret) {
         case ERR_NULL_PTR:
@@ -23,13 +28,9 @@ int main(void) {
         }
         return ret;
     }
-    for (uint8_t i = 0, *ii = grid; i < 4; ++i) {
-        for (uint8_t j = 0; j < 4; ++j, ++ii) {
-            if (*ii & EMPTY) {
-                printf("%s", "  _");
-            } else {
-                printf("%3hhu", *ii);
-            }
+    for (uint8_t i = 0, *ii = grid; i < 9; ++i) {
+        for (uint8_t j = 0; j < 9; ++j, ++ii) {
+            printf("%3hhu", *ii & ~EMPTY);
         }
         putchar(10);
     }
